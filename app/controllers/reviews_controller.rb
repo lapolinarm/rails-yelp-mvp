d10c2c16aea3b39c2fa0a7b7bc94d870a2581efb
Levi Apolinar <leviam51@gmail.com>
2024-08-28 16:57:18 -0500

    Challenge in progress - Advanced Routing - 2 - Yelp MVP

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,22 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new(review_params)
+    if @review.save
+      redirect_to @restaurant, notice: 'Review was successfully created.'
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def review_params
+  params.require(:review).permit(:rating, :content)
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+# Review.destroy_all
+
+puts "Creating Restaurants..."
+
+row1 = {name: "Restaurants 1", address: "av.restaurant 1", phone_number: "123456", category: "Category 1"}
+row2 = {name: "Restaurants 2", address: "av.restaurant 2", phone_number: "463453", category: "Category 2"}
+row3 = {name: "Restaurants 3", address: "av.restaurant 3", phone_number: "678334", category: "Category 3"}
+row4 = {name: "Restaurants 4", address: "av.restaurant 4", phone_number: "968643", category: "Category 4"}
+row5 = {name: "Restaurants 5", address: "av.restaurant 5", phone_number: "212344", category: "Category 5"}
+
+Restaurant.created(row1)
+Restaurant.created(row2)
+Restaurant.created(row3)
+Restaurant.created(row4)
+Restaurant.created(row5)
+
+puts "Finished!"
